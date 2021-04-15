fdd4fae53b91fe8831b045ed808b92301301de1e
romario <kids6352@gmail.com>
2021-04-14 20:12:39 -0400

    problema 3

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ if ( cont == 2 ) {
 } else {
     console.log( false );
 }
-*/
+
 // ejercicio 2 
 let texto: string = "this is a sample string";
 let textoInvertido: string = "";
@@ -24,3 +24,15 @@ for ( let  j = texto.length-1; j >= 0; j--) {
     textoInvertido = textoInvertido + texto.charAt( j );
 }
 console.log( textoInvertido );
+*/
+// ejercicio 3
+let cadena: string = "this is my example";
+let cadenaInvertido: string = "";
+for ( let k = cadena.length-1; k >= 0; --k) {
+    cadenaInvertido = cadenaInvertido + cadena.charAt( k );
+}
+if ( cadena == cadenaInvertido) {
+    console.log( true );  
+} else {
+    console.log( false );
+}
